f5ad434fb60d592bd9ec161c3b081c8b5b312ec2
Gustavo Enriquez Arias <gustavoenriquezarias@gmail.com>
2023-01-14 12:38:56 -0400

    CRUD-Completo-MySql

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 	"text/template"
@@ -28,24 +29,130 @@ var plantillas = template.Must(template.ParseGlob("c:/go/src/sistema/plantillas/
 func main() {
 	http.HandleFunc("/", Inicio)
 	http.HandleFunc("/registrar", Registrar)
+	http.HandleFunc("/insertar", Insertar)
+	http.HandleFunc("/eliminar", Eliminar)
+	http.HandleFunc("/editar", Editar)
+	http.HandleFunc("/actualizar", Actualizar)
 	log.Println("servidor corriendo...")
 	http.ListenAndServe(":8080", nil)
+}
+
+type Empleado struct {
+	Id     int
+	Nombre string
+	Correo string
 }
 
 func Inicio(w http.ResponseWriter, r *http.Request) {
 	//fmt.Fprintf(w, "Hola GusGus")
 	conexionEstablecida := conexionBD()
-	insertarRegistros, err := conexionEstablecida.Prepare("INSERT INTO empleados(nombre,correo) VALUES ('LULU','LULU@GMAIL.COM')")
+	consultarRegistros, err := conexionEstablecida.Query("SELECT * FROM empleados")
 
 	if err != nil {
 		panic(err.Error())
 	}
 
-	insertarRegistros.Exec()
-	plantillas.ExecuteTemplate(w, "inicio", nil)
+	empleado := Empleado{}
+	listaEmpleado := []Empleado{}
+
+	for consultarRegistros.Next() {
+		var id int
+		var nombre, correo string
+		err = consultarRegistros.Scan(&id, &nombre, &correo)
+		if err != nil {
+			panic(err.Error())
+		}
+		empleado.Id = id
+		empleado.Nombre = nombre
+		empleado.Correo = correo
+
+		listaEmpleado = append(listaEmpleado, empleado)
+	}
+
+	fmt.Println(listaEmpleado)
+
+	plantillas.ExecuteTemplate(w, "inicio", listaEmpleado)
 }
 
 func Registrar(w http.ResponseWriter, r *http.Request) {
 	//fmt.Fprintf(w, "Hola GusGus")
 	plantillas.ExecuteTemplate(w, "registrar", nil)
+}
+
+func Insertar(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		nombre := r.FormValue("nombre")
+		correo := r.FormValue("correo")
+		conexionEstablecida := conexionBD()
+		insertarRegistros, err := conexionEstablecida.Prepare("INSERT INTO empleados(nombre,correo) VALUES (?,?)")
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		insertarRegistros.Exec(nombre, correo)
+
+		http.Redirect(w, r, "/", 301)
+	}
+}
+
+func Eliminar(w http.ResponseWriter, r *http.Request) {
+	idEmpleado := r.URL.Query().Get("id")
+	fmt.Println(idEmpleado)
+	conexionEstablecida := conexionBD()
+	eliminarRegistro, err := conexionEstablecida.Prepare("DELETE FROM empleados WHERE id=?")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	eliminarRegistro.Exec(idEmpleado)
+
+	http.Redirect(w, r, "/", 301)
+}
+
+func Editar(w http.ResponseWriter, r *http.Request) {
+	idEmpleado := r.URL.Query().Get("id")
+	fmt.Println(idEmpleado)
+	conexionEstablecida := conexionBD()
+	consultarRegistros, err := conexionEstablecida.Query("SELECT * FROM empleados WHERE id=?", idEmpleado)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	empleado := Empleado{}
+
+	for consultarRegistros.Next() {
+		var id int
+		var nombre, correo string
+		err = consultarRegistros.Scan(&id, &nombre, &correo)
+		if err != nil {
+			panic(err.Error())
+		}
+		empleado.Id = id
+		empleado.Nombre = nombre
+		empleado.Correo = correo
+	}
+	fmt.Println(empleado)
+
+	plantillas.ExecuteTemplate(w, "editar", empleado)
+
+}
+
+func Actualizar(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		id := r.FormValue("id")
+		nombre := r.FormValue("nombre")
+		correo := r.FormValue("correo")
+		conexionEstablecida := conexionBD()
+		actualizarRegistros, err := conexionEstablecida.Prepare("UPDATE empleados SET nombre=?, correo=? WHERE id = ?")
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		actualizarRegistros.Exec(nombre, correo, id)
+
+		http.Redirect(w, r, "/", 301)
+	}
 }
